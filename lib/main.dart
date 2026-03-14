4f0d86bccb2c6c144e05110aa2aa8085e6c16267
Ahmed Khalid <105372011+ahmedroa@users.noreply.github.com>
2026-03-15 01:48:36 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:sada/features/home/widgets/bottom_nav_bar.dart';
 import 'package:sada/features/onboarding/onboarding.dart';
+import 'package:sada/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
@@ -27,8 +32,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
 
-      // home: const Onboarding(),
-      home: const BottomNavBar(),
+      home: const Onboarding(),
+      // home: const BottomNavBar(),
     );
   }
 }
